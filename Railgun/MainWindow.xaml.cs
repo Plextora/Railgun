f04337470db165b788a46d33b8958b1e00491132
Plextora <71889427+Plextora@users.noreply.github.com>
2023-06-20 12:08:45 -0400

    Add project files.

---
+++ Railgun/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿/*
+* Copyright 2023 Plextora
+* Licensed under the GPL-3.0 license (https://www.gnu.org/licenses/gpl-3.0.en.html#license-text)
+*/
+
+
+using System.Windows;
+
+namespace Railgun
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
