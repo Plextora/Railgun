9072c9d27b24b431225661f99441ce272b284ee3
Plextora <71889427+Plextora@users.noreply.github.com>
2023-07-17 11:53:33 -0400

    ClickEmulation class for autoclicker

---
+++ Railgun/Utils/Classes/ClickEmulation.cs
@@ -0,0 +1,48 @@
+﻿#region
+
+using System.Runtime.InteropServices;
+
+#endregion
+
+namespace Railgun.Utils.Classes
+{
+    public static class ClickEmulation
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool GetCursorPos(out MousePoint lpMousePoint);
+
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+
+        public static void SimulateMouseClick()
+        {
+            MousePoint mousePos = GetCursorPosition();
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, (uint)mousePos.X, (uint)mousePos.Y, 0, 0);
+        }
+
+        public static MousePoint GetCursorPosition()
+        {
+            bool gotPoint = GetCursorPos(out MousePoint currentMousePoint);
+            if (!gotPoint) currentMousePoint = new MousePoint(0, 0);
+            return currentMousePoint;
+        }
+
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MousePoint
+        {
+            public int X;
+            public int Y;
+
+            public MousePoint(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+        }
+    }
+}
